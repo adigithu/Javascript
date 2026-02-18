9e5c4659fdecf02972647cf883d7d5523e067eb4
Aditya J Parida <adityaparida605@gmail.com>
2026-02-18 19:44:15 +0530

    Variables in JS

---
+++ Basics/01_Variable.js
@@ -0,0 +1,9 @@
+const accountId=144553
+let accountEmail="adi@gmail.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+accountEmail="Aditya.com"
+accountPassword="21212121"
+accountCity="Adi"
+let account
+console.table([accountEmail,accountId,accountPassword,accountCity,account])
