eb1c55944a0e578835af3ffad4fcedbfb321ad3b
Aditya J Parida <adityaparida605@gmail.com>
2026-02-18 20:19:21 +0530

    Datatypes in JS

---
+++ Basics/02_Datatypes.js
@@ -0,0 +1,27 @@
+"use strict";  //Treat all Js code as newer version that is we will use all new syntax,classes and modules
+
+// alert(3+3)      We are using node js and not browser. 
+
+console.log(3+3); console.log("Adi")             //Not good in terms of code readability
+
+//TC39 and mdn js to be followed for documentation purposes
+
+let name="adi"   //String
+let age=21       //numeric
+let isLoggedIn=false  //boolean
+
+//List of Primitive data types
+
+// number =>  2 to the power 53
+//bigint => For bigger integers
+//string => ' ' or " "
+//boolean => true/false
+//null => It's a standalone value in JS. For example, let name=null (Empty)
+//undefined => When a value is not assigned. For ex:- let state (Here variable is declared but with no value being assigned it)
+//symbol => Uniqueness of components
+
+//Object
+
+console.log(typeof 'Adi')    //typeof: Returns the type of a variable or an expression
+console.log(typeof null)    //Object
+console.log(typeof undefined)   //Undefined because it's a type in JS
