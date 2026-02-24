2d19fac9c4043d9a1d0f8e0e6561fed190c45564
Aditya J Parida <adityaparida605@gmail.com>
2026-02-24 16:26:06 +0530

    Do While loop

---
+++ Looping/While.js
@@ -0,0 +1,23 @@
+// while (condition) {
+    
+// }
+
+let index=0
+while(index<=10){
+    console.log(`Value of index is ${index}`)
+    index=index+2
+}
+
+let myArray=["Adi", "Falesh", "MD"]
+let arr=0
+while(arr<myArray.length){
+    console.log(`Value is ${myArray[arr]}`)
+    arr=arr+1
+}
+
+//USe of do while loop 
+let score=11
+do{
+    console.log(`Score is ${score}`)  //11           Do-While loop ensures that it is executed atleast one
+    score++
+} while(score<=10)
